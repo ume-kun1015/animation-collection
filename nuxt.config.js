948dac62033315b07a2a91b4a6ef439dfac822fc
romukey <baymax.blackandwhite@gmail.com>
2020-11-22 14:41:23 +0900

    feat: use nuxtjs/svg

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,7 @@ export default {
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
     '@nuxtjs/eslint-module',
+    '@nuxtjs/svg',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
