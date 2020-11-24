a1a1766ea8b685965e470bdbe54fe2dbf8f5790c
romukey <baymax.blackandwhite@gmail.com>
2020-11-24 15:08:18 +0900

    feat: telemetry off

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
+  telemetry: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
