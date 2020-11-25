68c720a6d3f137d747235e2b4b04d23fc1237697
romukey <baymax.blackandwhite@gmail.com>
2020-11-25 14:57:50 +0900

    feat: impl ume-slide-menu

---
+++ pages/slide-menu/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="slide-menu-index">
+    <h1>slide-menu</h1>
+
+    <nuxt-link to="/" class="top-link">
+      <p>navigate to top page</p>
+    </nuxt-link>
+
+    <div class="button" @click="toggleShowSlideMenu">click here to toggle slide menu</div>
+
+    <ume-slide-menu v-model="showSlideMenu" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type Data = {
+  showSlideMenu: boolean
+}
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      showSlideMenu: false,
+    }
+  },
+
+  methods: {
+    toggleShowSlideMenu() {
+      this.showSlideMenu = !this.showSlideMenu
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.slide-menu-index {
+  padding: 20px;
+
+  .top-link {
+    display: block;
+    margin: 20px 0;
+  }
+}
+</style>
