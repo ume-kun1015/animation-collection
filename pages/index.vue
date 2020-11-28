38fde1c9f9a3878a95c734b1bef5ff3901514308
romukey <baymax.blackandwhite@gmail.com>
2020-11-28 15:46:20 +0900

    fix style

--- pages/index.vue
+++ pages/index.vue
@@ -38,7 +38,12 @@ export default Vue.extend({
 
     li {
       width: 30%;
-      margin: 20px;
+      margin: 16px;
+
+      @media only screen and (max-width: $breakpoint-mobile) {
+        width: 42%;
+        margin: 12px;
+      }
 
       .card {
         display: block;
@@ -47,10 +52,15 @@ export default Vue.extend({
         box-shadow: 0 6px 10px rgba(0, 0, 0, 0.08), 0 0 6px rgba(0, 0, 0, 0.05);
         transition: 0.3s transform cubic-bezier(0.155, 1.105, 0.295, 1.12), 0.3s box-shadow,
           0.3s -webkit-transform cubic-bezier(0.155, 1.105, 0.295, 1.12);
-        padding: 14px 80px 18px 36px;
+        padding: 16px;
         cursor: pointer;
         text-decoration: none;
         height: 60px;
+
+        @media only screen and (max-width: $breakpoint-mobile) {
+          padding: 16px;
+          height: 80px;
+        }
 
         &:hover {
           transform: scale(1.05);
